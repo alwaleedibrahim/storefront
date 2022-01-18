83b68dd00a627f30658a407a5a875cc29fc26895
Alwaleed Ibrahim <alwaleedibramohammed@gmail.com>
2022-01-18 20:24:15 +0200

    Added product model and handler

---
+++ src/handlers/product.ts
@@ -0,0 +1,48 @@
+import express, {Request, Response} from "express"
+import {Product, ProductStore} from "../models/product"
+
+const product_store = new ProductStore()
+
+const index = async (req: Request, res: Response) => {
+    try {
+        const products = await product_store.index()
+        res.send(products)
+    }
+    catch(err) {
+        res.status(500).json(err)
+    }
+}
+
+const show = async (req: Request, res: Response) => {
+    try {
+        const product = await product_store.show(Number(req.params.id))
+        res.send(product)
+    }
+    catch(err) {
+        res.status(500).json(err)
+    }
+}
+
+const create = async (req: Request, res: Response) => {
+    const product: Product = {
+        id: NaN,
+        name: req.body.name,
+        price: req.body.price
+    }
+    try {
+        const productCreated = await product_store.create(product)
+        res.send(productCreated)
+    }
+    catch(err) {
+        res.status(500).json(err)
+    }
+}
+
+
+const productRoutes = (app: express.Application) => {
+    app.get("/products", index)
+    app.get("/products/:id", show)
+    app.post("/products", create)
+}
+
+export default productRoutes
