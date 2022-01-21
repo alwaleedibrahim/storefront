0a1537f1f76a0db36946519262052aade1c8d6ca
Alwaleed Ibrahim <alwaleedibramohammed@gmail.com>
2022-01-21 15:01:07 +0200

    Added orders handler

---
+++ src/handlers/order.ts
@@ -0,0 +1,62 @@
+import express, {Request, Response} from "express"
+import {Order, OrderProduct, OrderStore} from "../models/order"
+
+const order_store = new OrderStore()
+
+const create = async(req: Request, res: Response) => {
+    const order: Order = {
+        id:NaN,
+        status: req.body.status,
+        user_id: req.body.user_id
+    }
+    try{
+        const orderCreated = await order_store.create(order)
+        res.send(orderCreated)
+    }
+    catch(err) {
+        res.status(500).json(err)
+    }
+}
+
+const addProduct = async(req: Request, res: Response) => {
+    const orderProduct: OrderProduct = {
+        id: NaN,
+        order_id: req.body.order_id,
+        product_id: req.body.product_id,
+        quantity: req.body.quantity
+    }
+    try{
+        const addedProduct = await order_store.addProduct(orderProduct)
+        res.send(addedProduct)
+    }
+    catch(err) {
+        res.status(500).json(err)
+    }
+}
+
+const ShowOrdersByUser = async(req: Request, res: Response) => {
+    try{
+        const ordersByUser = await order_store.ShowOrdersByUser(Number(req.params.id))
+        res.send(ordersByUser)
+    }
+    catch(err) {
+        res.status(500).json(err)
+    }
+}
+
+const showCurrentOrder = async(req: Request, res: Response) => {
+    try {
+        const currentOrder = await order_store.currentOrder(Number(req.body.user_id))
+        res.send(currentOrder)
+    }
+    catch(err) {
+        res.status(500).json(err)
+    }
+}
+
+const orderRouters = (app: express.Application) => {
+    app.post("/orders", create)
+    app.post("/orders/addProduct", addProduct)
+    app.get("/orders/user/:id", ShowOrdersByUser)
+    app.get("/orders/user/:id/current", showCurrentOrder)
+}
